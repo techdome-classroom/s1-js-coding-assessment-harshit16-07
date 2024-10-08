46cc4cf3110f7f0d00cca24124ed60d5d18ced5e
harshit16-07 <162592465+harshit16-07@users.noreply.github.com>
2024-10-08 12:19:17 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,20 @@
 const decodeTheRing = function (s, p) {
+    const isMatch = (i, j) => {
+        if (i >= s.length && j >= p.length) return true;
+        if (j >= p.length) return false;
 
-    // write your code here
+        if (p[j] === '*') {
+            return isMatch(i, j + 1) || (i < s.length && isMatch(i + 1, j));
+        }
 
-  };
-  
-  module.exports = decodeTheRing;
+        if (i < s.length && (p[j] === '?' || p[j] === s[i])) {
+            return isMatch(i + 1, j + 1);
+        }
+
+        return false;
+    };
+
+    return isMatch(0, 0);
+};
+
+module.exports = decodeTheRing;
